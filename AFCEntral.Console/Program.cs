d143e058027a5bc63a9fb56e0d8770165295d3f9
barbara-tshimologong <barbara@tshimologong.joburg>
2022-05-20 12:26:13 +0200

    Add project files.

---
+++ AFCEntral.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DataAccess;
+
+namespace AFCEntral.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            SqlServerAccess ssa = new SqlServerAccess();
+            ssa.MyConnectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = AFCentral; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+            var replyCount = ssa.SqlAccessConnect();
+
+            System.Console.WriteLine($"The number of rows in the Person table is: { replyCount}");
+        }
+  }
+}
---
+++ DataAccess/SqlServerAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DataAccess
+{
+    public class SqlServerAccess
+    {
+
+        public string MyConnectionString { get; set; }
+
+        public int SqlAccessConnect()
+        {
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = MyConnectionString;
+                cn.Open();
+
+                using (SqlCommand cm = new SqlCommand())
+                {
+                    cm.Connection = cn;
+
+                    cm.CommandText = "SELECT Count(*) FROM Member";
+                    cm.CommandType = CommandType.Text;
+
+                    var reply = (int)cm.ExecuteScalar();
+
+                    return reply;
+                }
+            }
+        }
+    }
+}
